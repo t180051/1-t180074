5001556a0ff9ac3cce06ed382c89732f84eb9d09
t180074 <49540847+t180074@users.noreply.github.com>
2020-04-13 11:37:06 +0900

    Update kadai1-2.c

--- kadai1-2.c
+++ kadai1-2.c
@@ -5,7 +5,7 @@ int main() {
     int data[] = {31, 41, 59, 26, 53, 58, 97, 93, 23, 84};
     int max_data = data[0];
 
-    for(i = /* ここを埋める */; i < sizeof(data) / sizeof(data[0]); i++) {
+    for(i = 1/; i < sizeof(data) / sizeof(data[0]); i++) {
     }
 
     printf("最大値は %d です。\n", max_data);
